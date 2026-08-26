2288ac4df7b7c1cba1305d8700d6104303b6bd16
junayedhasan302 <junayedhasan302@gmail.com>
2026-08-27 03:59:36 +0600

    9_Palindrome

---
+++ 9_Palindrome.js
@@ -0,0 +1,15 @@
+// JUNAYED HASAN
+
+const isPalindrome = (x) => {
+    if (x < 0) {
+        return false;
+    }
+    let original = x;
+    let reverse = 0;
+    while (x > 0) {
+        let digit = x % 10;
+        reverse = reverse * 10 + digit;
+        x = Math.floor(x / 10);
+    }
+    return original === reverse;
+};
