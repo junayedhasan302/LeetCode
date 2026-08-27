6383ea0fe63035b4d952c3f5b1ab3404950fd411
junayedhasan302 <junayedhasan302@gmail.com>
2026-08-28 02:24:05 +0600

    58 Length of last word ES6 format

--- 58_Length_of_last_word.js
+++ 58_Length_of_last_word.js
@@ -1,16 +1,8 @@
-let lengthOfLastWord = function(s) {
+const lengthOfLastWord = (s) => {
     let count = 0;
-    // Start from the last character
     for (let i = s.length - 1; i >= 0; i--) {
-        // Ignore spaces at the end
-        if (s[i] === ' ') {
-            if (count > 0) {
-                break;
-            }
-        } 
-        else {
-            count++;
-        }
+        if (s[i] === ' ' && count > 0) break;
+        if (s[i] !== ' ') count++;
     }
     return count;
 };
