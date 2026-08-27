b07ea770e58267430ee429f55ca459e03d665665
junayedhasan302 <junayedhasan302@gmail.com>
2026-08-28 03:03:30 +0600

    66 Plus One- Accepted

--- 66_Plus_One.js
+++ 66_Plus_One.js
@@ -1,7 +1,16 @@
 // JUNAYED HASAN
+// 1st approach
+// const plusOne = (digits) => {
+//     const arr = [...digits];
+//     const result = Number(arr.join('')) + 1;
+//     return String(result).split('').map(Number);
+// };
+// console.log(plusOne([1, 2, 3]));
+
+
 const plusOne = (digits) => {
     const arr = [...digits];
-    const result = Number(arr.join('')) + 1;
+    const result = BigInt(arr.join('')) + 1n;
     return String(result).split('').map(Number);
 };
 console.log(plusOne([1, 2, 3]));
